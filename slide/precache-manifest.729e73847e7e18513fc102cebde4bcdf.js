012dda71294628d40fcc0d628af48ffe29d552d7
蜗牛mr <364846148@qq.com>
2019-05-09 17:08:35 +0800

    react轮播图第一次提交

---
+++ slide/precache-manifest.729e73847e7e18513fc102cebde4bcdf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a96d75022079fb82a22a76a393f3f40",
+    "url": "./static/media/5.5a96d750.jpg"
+  },
+  {
+    "revision": "513dff4fac76b0e43d38b031a77b7019",
+    "url": "./static/media/4.513dff4f.jpg"
+  },
+  {
+    "revision": "340ac8a0046a13f775e98e74dc53340c",
+    "url": "./static/media/3.340ac8a0.jpg"
+  },
+  {
+    "revision": "cc542e71038f925080e693eac5ee8c64",
+    "url": "./static/media/2.cc542e71.jpg"
+  },
+  {
+    "revision": "a65426e3637fe12cd39368185501a66b",
+    "url": "./static/media/1.a65426e3.jpg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "40fe540303359c2749fd",
+    "url": "./static/js/main.958f328c.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "40fe540303359c2749fd",
+    "url": "./static/css/main.db3d871e.chunk.css"
+  },
+  {
+    "revision": "cda1caadcf0e5a28cb960eb1c6d8f14c",
+    "url": "./index.html"
+  }
+];
